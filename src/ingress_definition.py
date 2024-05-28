c125d0328644dfc74f5dbe4a8dc5a66555af104a
Trung Thanh Phan <trung.thanh.phan@canonical.com>
2024-05-28 11:46:27 +0200

    remove consts file

--- src/ingress_definition.py
+++ src/ingress_definition.py
@@ -7,8 +7,6 @@ from typing import Union
 
 import ops
 
-from consts import BOOLEAN_CONFIG_FIELDS
-
 
 def get_config(charm: ops.CharmBase, field: str) -> Union[str, float, int, bool, None]:
     """Get data from charm config.
@@ -22,10 +20,4 @@ def get_config(charm: ops.CharmBase, field: str) -> Union[str, float, int, bool,
     """
     # Config fields with a default of None don't appear in the dict
     config_data = charm.config.get(field, None)
-    # A value of False is valid in these fields, so check it's not a null-value instead
-    if field in BOOLEAN_CONFIG_FIELDS and (config_data is not None and config_data != ""):
-        return config_data
-    if config_data:
-        return config_data
-
-    return None
+    return config_data

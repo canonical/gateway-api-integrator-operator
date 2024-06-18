ea7ab514f4c3a3ab4838288c29a9326e818d459c
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2024-06-18 11:02:27 +0200

    intentionally failling tests

--- tests/integration/test_certificates_integration.py
+++ tests/integration/test_certificates_integration.py
@@ -22,7 +22,7 @@ async def test_certificates_relation(
 
     Assert on the unit status before any relations/configurations take place.
     """
-    await application.set_config({"external-hostname": TEST_EXTERNAL_HOSTNAME_CONFIG})
+    # await application.set_config({"external-hostname": TEST_EXTERNAL_HOSTNAME_CONFIG})
     await application.model.add_relation(application.name, certificate_provider_application.name)
     await application.model.wait_for_idle(
         apps=[application.name, certificate_provider_application.name],

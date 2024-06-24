dff5f7fb177bc86fe00d25d2928706b24c65cbe4
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-06-25 00:00:50 +0200

    fix typo

--- src/state/tls.py
+++ src/state/tls.py
@@ -57,7 +57,10 @@ class TLSInformation:
             TLSInformation: Information about configured TLS certs.
         """
         tls_requirer_integration = charm.model.get_relation(TLS_CERTIFICATES_INTEGRATION)
-        if tls_requirer_integration is None or tls_requirer_integration.get(charm.app) is None:
+        if (
+            tls_requirer_integration is None
+            or tls_requirer_integration.data.get(charm.app) is None
+        ):
             raise TlsIntegrationMissingError("Certificates integration not ready.")
 
         tls_certs = {}

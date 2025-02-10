b89e6feadfcb6d5e7d34a5288f79a315d7beb633
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2025-02-10 16:24:02 +0100

    Revert "update copyright header"
    This reverts commit b81561de9c876cb911201683eb7211c5bbec83a6.

--- tests/unit/test_http_route.py
+++ tests/unit/test_http_route.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 # Disable protected access rules to test charm._labels and charm._ingress_provider

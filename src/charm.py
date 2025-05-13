3e540e676a4971a3c263e5939aed6072c2112a30
DeeKay3 <dkamar490@gmail.com>
2025-05-13 08:11:46 +0100

    feat/ISD-3237: adding dns record

--- src/charm.py
+++ src/charm.py
@@ -6,7 +6,15 @@
 
 import logging
 import typing
+import uuid
 
+from charms.bind.v0.dns_record import (
+    DNSRecordRequirerData,
+    DNSRecordRequires,
+    RecordClass,
+    RecordType,
+    RequirerEntry,
+)
 from charms.tls_certificates_interface.v3.tls_certificates import (
     AllCertificatesInvalidatedEvent,
     CertificateAvailableEvent,
@@ -51,6 +59,7 @@ logger = logging.getLogger(__name__)
 CREATED_BY_LABEL = "gateway-api-integrator.charm.juju.is/managed-by"
 INGRESS_RELATION = "gateway"
 TLS_CERT_RELATION = "certificates"
+UUID_NAMESPACE = uuid.UUID("f8f206da-a7f8-4206-b044-30be3724a09d")
 
 
 def _get_client(field_manager: str, namespace: str) -> Client:
@@ -96,6 +105,7 @@ class GatewayAPICharm(CharmBase):
         self.certificates = TLSCertificatesRequiresV3(self, TLS_CERT_RELATION)
         self._ingress_provider = IngressPerAppProvider(charm=self, relation_name=INGRESS_RELATION)
         self._tls = TLSRelationService(self.model, self.certificates)
+        self.dns_record_requirer = DNSRecordRequires(self)
 
         self.framework.observe(self.on.config_changed, self._on_config_changed)
         self.framework.observe(self.on.start, self._on_start)
@@ -126,6 +136,13 @@ class GatewayAPICharm(CharmBase):
 
         self.framework.observe(self._ingress_provider.on.data_provided, self._on_data_provided)
         self.framework.observe(self._ingress_provider.on.data_removed, self._on_data_removed)
+
+        self.framework.observe(
+            self.on.dns_record_relation_created, self._on_dns_record_relation_created
+        )
+        self.framework.observe(
+            self.on.dns_record_relation_joined, self._on_dns_record_relation_joined
+        )
 
     @property
     def _labels(self) -> dict[str, str]:
@@ -249,6 +266,16 @@ class GatewayAPICharm(CharmBase):
         """Handle the data-removed event."""
         self._reconcile()
 
+    @validate_config_and_integration(defer=False)
+    def _on_dns_record_relation_created(self, _: RelationCreatedEvent) -> None:
+        """Handle the DNS record relation created event."""
+        self._reconcile()
+
+    @validate_config_and_integration(defer=False)
+    def _on_dns_record_relation_joined(self, _: RelationJoinedEvent) -> None:
+        """Handle the DNS record relation joined event."""
+        self._reconcile()
+
     def _reconcile(self) -> None:
         """Reconcile charm status based on configuration and integrations.
 
@@ -262,10 +289,43 @@ class GatewayAPICharm(CharmBase):
             4. Publish the ingress URL to the requirer charm.
             5. Set the gateway LB address in the charm's status message.
         """
+
         client = _get_client(field_manager=self.app.name, namespace=self.model.name)
         config = CharmConfig.from_charm(self, client)
+        logger.info(config)
+        logger.info(self.config)
         gateway_resource_information = GatewayResourceInformation.from_charm(self)
         tls_information = TLSInformation.from_charm(self, self.certificates)
+
+        # ToDo: If relation is established AND external_hostname is configured AND ip address in gateway => request for record
+        resource_manager = GatewayResourceManager(
+            labels=self._labels,
+            client=client,
+        )
+        self.unit.status = WaitingStatus("Waiting for gateway address")
+        gateway_address = resource_manager.gateway_address(
+            gateway_resource_information.gateway_name
+        )
+        relation = self.model.get_relation(self.dns_record_requirer.relation_name)
+        if (
+            relation
+            and config.external_hostname
+            and gateway_address  # ToDo: Check if ip address in gateway
+        ):
+            host_label, domain = config.external_hostname.split(".", 1)  # www.example.com
+            entry = RequirerEntry(
+                domain=domain,
+                host_label=host_label,
+                ttl=600,
+                record_class=RecordClass.IN,
+                record_type=RecordType.A,
+                record_data=gateway_address,
+                uuid=uuid.uuid5(
+                    UUID_NAMESPACE, str(config.external_hostname) + str(gateway_address)
+                ),
+            )
+            dns_record_requirer_data = DNSRecordRequirerData(dns_entries=[entry])
+            self.dns_record_requirer.update_relation_data(relation, dns_record_requirer_data)
 
         self.unit.status = MaintenanceStatus("Creating resources.")
         self._define_gateway_resource(
@@ -276,6 +336,15 @@ class GatewayAPICharm(CharmBase):
             client, config, gateway_resource_information
         )
         self._set_status_gateway_address(client, gateway_resource_information)
+
+    def _get_dns_record_requirer_data(self) -> DNSRecordRequirerData:
+        """Get the DNS record requirer data.
+
+        Returns:
+            DNSRecordRequirerData: The DNS record requirer data.
+        """
+
+        return DNSRecordRequirerData(dns_entries=[])  # ToDo: Fill here
 
     def _define_gateway_resource(
         self,

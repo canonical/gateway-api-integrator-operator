058e4ee5605ca8e84aa3edef190a1b712cbfd679
Ali Ugur <ali.ugur@canonical.com>
2026-01-12 10:29:48 +0300

    Chore: Update tests

--- tests/e2e/test_configurator.py
+++ tests/e2e/test_configurator.py
@@ -8,6 +8,7 @@ import socket
 import jubilant
 import requests
 import urllib3
+import yaml
 from urllib3.exceptions import InsecureRequestWarning
 
 from .conftest import App  # pylint: disable=no-name-in-module
@@ -15,7 +16,24 @@ from .conftest import App  # pylint: disable=no-name-in-module
 # Disable SSL warnings when using verify=False
 urllib3.disable_warnings(InsecureRequestWarning)
 
-
+def get_url_from_relation(juju: jubilant.Juju, unit_name: str) -> str:                                                                                                                        
+    """Get the ingress url from the units relation data.                                                                                                                                      
+                                                                                                                                                                                              
+    Args:                                                                                                                                                                                     
+        juju (jubilant.Juju): The jubilant Juju instance.                                                                                                                                     
+        unit_name (str): The target unit's name.                                                                                                                                              
+                                                                                                                                                                                              
+    Returns:                                                                                                                                                                                  
+        str: The ingress IP address.                                                                                                                                                          
+    """                                                                                                                                                                                       
+    unit_data = yaml.load(juju.cli("show-unit", unit_name), Loader=yaml.CLoader)                                                                                                              
+                                                                                                                                                                                              
+    for relation in unit_data[unit_name]["relation-info"]:                                                                                                                                    
+        if relation["endpoint"] == "ingress":                                                                                                                                                 
+            # app data is encoded as a string so we have to load it as yaml again :(                                                                                                          
+            return yaml.load(relation["application-data"]["ingress"], Loader=yaml.CLoader)["url"]                                                                                             
+    return ""                                                                                                                                                                                 
+              
 def get_gateway_ip(juju: jubilant.Juju, gateway_api_integrator: App) -> str:
     """Get the gateway IP from the charm status message.
 
@@ -104,3 +122,4 @@ def test_configurator(
     response = requests.get(url, verify=False, timeout=10)  # nosec
     assert response.status_code == 200
     assert "Welcome to flask-k8s Charm" in response.text
+    assert get_url_from_relation(juju, "flask-k8s/0") == url

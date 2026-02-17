91a4ef9d5de666f78a08dae8114dd2b2e21b10ea
tphan025 <trung.thanh.phan@canonical.com>
2026-02-17 21:23:34 +0100

    add pytest options

---
+++ gateway-api-integrator/tests/conftest.py
@@ -0,0 +1,14 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Fixtures for charm tests."""
+
+
+def pytest_addoption(parser):
+    """Parse additional pytest options.
+
+    Args:
+        parser: Pytest parser.
+    """
+    parser.addoption("--charm-file", action="append")
+    parser.addoption("--kube-config", action="store", default="~/.kube/config")

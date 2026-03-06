b7b8378042dbac07eb47258649066f283f5e08c6
Ali Ugur <ali.ugur@canonical.com>
2026-03-06 10:33:19 +0300

    Chore: Move to charmlibs TLS

--- gateway-api-integrator/src/charm.py
+++ gateway-api-integrator/src/charm.py
@@ -22,7 +22,7 @@ from charms.gateway_api_integrator.v0.gateway_route import (
     GatewayRouteDataRemovedEvent,
     GatewayRouteProvider,
 )
-from charms.tls_certificates_interface.v4.tls_certificates import (
+from charmlibs.interfaces.tls_certificates import (
     CertificateAvailableEvent,
     CertificateRequestAttributes,
     Mode,

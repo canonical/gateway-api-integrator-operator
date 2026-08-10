e210d5b912fb1bb8d13ecbeaa830572f71a26862
Fouad Atwi <fouad.atwi@canonical.com>
2026-08-10 10:39:50 +0200

    fix typo

--- terraform/product/outputs.tf
+++ terraform/product/outputs.tf
@@ -3,7 +3,7 @@
 
 output "gateway_api_integrator_app_name" {
   description = "Name of the deployed gateway-api-integrator application."
-  value       = module.gateway_api_integrator.app_nam
+  value       = module.gateway_api_integrator.app_name
 }
 
 output "ingress_configurator_app_name" {
